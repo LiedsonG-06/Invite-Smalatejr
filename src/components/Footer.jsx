32867ae0b7484dc187ea2ee916927181f3a77fa6
Liedson Gove <liedsongove353@gmail.com>
2026-09-18 11:08:57 +0200

    gg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,2 @@
 ﻿import { invitationData as data } from '../data/invitationData'
-export function Footer() { const { couple, displayDate, finalMessage, images } = data; return <footer style={{ '--section-image': `url(${images.hero.src})` }}><div className="monogram">{couple.monogram}</div><p>{finalMessage}</p><small>{displayDate} · Feito por Engenheiro Liedson Gove</small></footer> }
+export function Footer() { const { couple, displayDate, finalMessage, images } = data; return <footer style={{ '--section-image': `url(${images.hero.src})` }}><div className="monogram">{couple.monogram}</div><p>{finalMessage}</p><small>{displayDate} · Feito por Liedson Gove</small></footer> }
